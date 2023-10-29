891ab3103320694f0d48a83b33030832d4909537
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-10-28 22:00:49 -0300

    feat: home page and some imports update

---
+++ src/app/heroes/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div
+        className="z-10
+          justify-between
+          w-full
+          max-w-5xl
+          font-mono
+          text-sm
+          items-center
+          lg:flex"
+      >
+        <h1>Teste 22</h1>
+      </div>
+    </main>
+  );
+}
