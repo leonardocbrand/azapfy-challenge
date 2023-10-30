03c3615bcf2c940b5dd25e77e511b05dcb61ff37
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-10-30 01:40:07 -0300

    fix: heroes store

--- src/store/heroes.tsx
+++ src/store/heroes.tsx
@@ -5,10 +5,14 @@ type StoreProps = {
   state: {
     heroes: Heroes[],
     filteredHeroes: Heroes[]
+    selectedHeroes: Heroes[]
+    modal: boolean
   },
   actions: {
     setHeroes: (data: Heroes[]) => void
     setFilteredHeroes: (data: Heroes[]) => void
+    setSelectedHeroes: (data: Heroes[]) => void
+    setModal: (modalOpen: boolean) => void
   }
 };
 
@@ -16,10 +20,20 @@ export const useHeroesStore = create<StoreProps>((set) => ({
   state: {
     heroes: [],
     filteredHeroes: [],
+    selectedHeroes: [],
+    modal: false,
   },
   actions: {
-    setHeroes: (data: Heroes[]) => set({ state: { heroes: data, filteredHeroes: data } }),
+    setHeroes: (data: Heroes[]) => set(
+      { state:
+        { heroes: data, filteredHeroes: data, selectedHeroes: [], modal: false } },
+    ),
     setFilteredHeroes: (data: Heroes[]) => set((state) => (
       { state: { ...state.state, filteredHeroes: data } })),
+    setSelectedHeroes: (data: Heroes[]) => set((state) => (
+      { state: { ...state.state, selectedHeroes: data } })),
+    setModal: (modalOpen: boolean) => set((state) => (
+      { state: { ...state.state, modal: modalOpen } })),
   },
+
 }));
