c33cb5b0eaaec9347b68e22592be326f1dc7eca1
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-10-31 12:01:26 -0300

    fix typo

--- src/services/getHeroes.ts
+++ src/services/getHeroes.ts
@@ -1,7 +1,7 @@
 import { Heroes } from '@/app/types';
 
 const getHeroes = async (): Promise<Heroes[]> => {
-  const response = await fetch('//homologacao3.azapfy.com.br/api/ps/metahumans');
+  const response = await fetch('http://homologacao3.azapfy.com.br/api/ps/metahumans');
   const data = response.json();
 
   return data;
